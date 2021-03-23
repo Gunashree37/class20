58abe607771919c230692196e0eedc64b6baa8cc
Gunashree S <gunashrees341@gmail.com>
2021-03-23 07:20:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,12 @@
-var fixedRect,movingRect;
 
 function setup() {
   createCanvas(800,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "black";
-
-  movingRect = createSprite(400,200,80,30);
-  movingRect.shapeColor = "blue";
+  
 }
 
 function draw() {
   background(100,80,50);
   
-  movingRect.x = mouseX;
-  movingRect.y = mouseY;
-
-  if(fixedRect.x - movingRect.x < fixedRect.width/2+movingRect.width/2
-    && movingRect.x - fixedRect.x < fixedRect.width/2+movingRect.width/2
-    &&fixedRect.y - movingRect.y < fixedRect.height/2+movingRect.height/2
-    && movingRect.y - fixedRect.y < fixedRect.height/2+movingRect.height/2){
-    fixedRect.shapeColor = "green";
-    movingRect.shapeColor = "green";
-  }
-  else{
-    fixedRect.shapeColor = "black";
-    movingRect.shapeColor = "blue";
-  }
-
+ 
   drawSprites();
 }
